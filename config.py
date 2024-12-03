feea50a82d0c9b1205c1041df0ce2a7cf71457ef
MN - TG <mntg3509@gmail.com>
2024-12-03 14:59:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+CAPTION = environ.get("CAPTION", "hi")
 SOURCE_CHANNEL = -1002345447637 # Replace with your source channel ID
 TARGET_CHANNEL = -1002490892111 # Replace with your target channel ID
 
