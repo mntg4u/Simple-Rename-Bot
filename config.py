a1ed6972b823b4056a0659a3e280676e9c798347
MN - TG <mntg3509@gmail.com>
2024-12-03 14:56:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
-SOURCE_CHANNEL = -1001234567890  # Replace with your source channel ID
-TARGET_CHANNEL = -1009876543210  # Replace with your target channel ID
+SOURCE_CHANNEL = -1002345447637 # Replace with your source channel ID
+TARGET_CHANNEL = -1002490892111 # Replace with your target channel ID
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
