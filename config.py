70a0a5639d1752cdaba468290edc4a8ca504bdc1
MN - TG <mntg3509@gmail.com>
2024-12-03 15:01:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "22980696"))
+API_HASH = environ.get("API_HASH", "2b653cb53821a82097efaba6732f5d75")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7421263728:AAGVeMyCLHeX053qbhsopwiRG-hhwNFPAzc")
+ADMIN = int(environ.get("ADMIN", "1892771262"))          
 CAPTION = environ.get("CAPTION", "hi")
 SOURCE_CHANNEL = -1002345447637 # Replace with your source channel ID
 TARGET_CHANNEL = -1002490892111 # Replace with your target channel ID
